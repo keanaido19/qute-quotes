29c002fa26d0f2b6dffcca424ffd79b729269854
keanaido021@ <keanaido021@student.wethinkcode.co.za>
2022-07-16 15:31:56 +0200

    Documented the API

--- Server/src/main/java/de/wethinkco/qutequotes/api/ApiServer.java
+++ Server/src/main/java/de/wethinkco/qutequotes/api/ApiServer.java
@@ -1,6 +1,12 @@
 package de.wethinkco.qutequotes.api;
 
 import io.javalin.Javalin;
+import io.javalin.plugin.openapi.OpenApiOptions;
+import io.javalin.plugin.openapi.OpenApiPlugin;
+import io.javalin.plugin.openapi.ui.ReDocOptions;
+import io.javalin.plugin.openapi.ui.SwaggerOptions;
+import io.swagger.v3.oas.models.info.Info;
+import org.slf4j.simple.SimpleLogger;
 
 import static io.javalin.apibuilder.ApiBuilder.*;
 
@@ -8,9 +14,13 @@ public class ApiServer {
     private final Javalin server;
 
     public ApiServer() {
+        System.setProperty(SimpleLogger.DEFAULT_LOG_LEVEL_KEY, "ERROR");
         this.server = Javalin
                 .create(
-                        config -> config.defaultContentType = "application/json"
+                        config -> {
+                            config.registerPlugin(getConfiguredOpenApiPlugin());
+                            config.defaultContentType = "application/json";
+                        }
                 )
                 .routes(() -> {
                             path("quote",
@@ -29,11 +39,39 @@ public class ApiServer {
     }
 
     public Javalin start() {
+        System.out.println(
+                "Welcome to Qute Quotes Server!\n" +
+                        "Server is listening on...\n" +
+                        "\tAddress\t\t:\thttp://localhost:5000/\n" +
+                        "\tReDocs\t\t:\thttp://localhost:5000/redoc\n" +
+                        "\tSwagger UI\t:\thttp://localhost:5000/" +
+                        "swagger-ui"
+        );
         return this.server.start(5000);
     }
 
     public Javalin stop() {
         return this.server.stop();
+    }
+
+    private static OpenApiPlugin getConfiguredOpenApiPlugin() {
+        Info info =
+                new Info()
+                        .version("1.0")
+                        .title("Qute Quotes API")
+                        .description(
+                                "This API provides endpoints to get and " +
+                                        "create quotes."
+                        );
+
+        OpenApiOptions options = new OpenApiOptions(info)
+                .activateAnnotationScanningFor(
+                        "de.wethinkco.qutequotes.api"
+                )
+                .path("/swagger-docs")
+                .swagger(new SwaggerOptions("/swagger-ui"))
+                .reDoc(new ReDocOptions("/redoc"));
+        return new OpenApiPlugin(options);
     }
 
     public static void main(String[] args) {
--- Server/src/main/java/de/wethinkco/qutequotes/api/QuoteController.java
+++ Server/src/main/java/de/wethinkco/qutequotes/api/QuoteController.java
@@ -1,33 +1,133 @@
 package de.wethinkco.qutequotes.api;
 
-import de.wethinkco.qutequotes.quote.Quote;
 import de.wethinkco.qutequotes.dbconnect.QuoteService;
-import io.javalin.http.Context;
-import io.javalin.http.HttpCode;
-
-import java.sql.SQLException;
+import de.wethinkco.qutequotes.quote.Quote;
+import io.javalin.http.*;
+import io.javalin.plugin.openapi.annotations.*;
 
 public class QuoteController {
 
-    public static  void getAll(Context context) throws SQLException {
-        context.json(QuoteService.getAll());
+    @OpenApi(
+            summary = "Get all quotes.",
+            operationId = "getAllQuotes",
+            path = "/quotes",
+            method = HttpMethod.GET,
+            tags = {"Quote"},
+            responses = {
+                    @OpenApiResponse(
+                            status = "200",
+                            content = {
+                                    @OpenApiContent(
+                                            from = Quote[].class
+                                    )
+                            }
+                    ),
+                    @OpenApiResponse(
+                            status = "500",
+                            content = {
+                                    @OpenApiContent(
+                                            from = ErrorResponse.class
+                                    )
+                            }
+                    )
+            }
+
+    )
+    public static  void getAll(Context context) {
+        try {
+            context.json(QuoteService.getAll());
+        } catch (Exception e) {
+            throw new InternalServerErrorResponse();
+        }
     }
 
-    public static void getOne(Context context) throws SQLException {
-        Integer id = context.pathParamAsClass("id", Integer.class).get();
-        Quote quote = QuoteService.findById(id);
-        context.json(quote);
+    @OpenApi(
+            summary = "Get a quote by ID.",
+            operationId = "getQuoteByID",
+            path = "/quote/{id}",
+            method = HttpMethod.GET,
+            pathParams = {
+                    @OpenApiParam(
+                            name = "id",
+                            type = Integer.class,
+                            description = "The quote ID"
+                    )
+            },
+            tags = {"Quote"},
+            responses = {
+                    @OpenApiResponse(
+                            status = "200",
+                            content = {
+                                    @OpenApiContent(
+                                            from = Quote.class
+                                    )
+                            }
+                    ),
+                    @OpenApiResponse(
+                            status = "404",
+                            content = {
+                                    @OpenApiContent(
+                                            from = ErrorResponse.class
+                                    )
+                            }
+                    )
+            }
+
+    )
+    public static void getOne(Context context) {
+        try {
+            Integer id = context.pathParamAsClass("id", Integer.class).get();
+            Quote quote = QuoteService.findById(id);
+            context.json(quote);
+        } catch (Exception e) {
+            throw new NotFoundResponse();
+        }
     }
 
-    public static void create(Context context) throws SQLException {
-        QuoteRequest quoteRequest = context.bodyAsClass(QuoteRequest.class);
-        Quote quote =
-                QuoteService.save(
-                        quoteRequest.getText(),
-                        quoteRequest.getName()
-                );
-        context.header("Location", "/quote/" + quote.getId());
-        context.status(HttpCode.CREATED);
-        context.json(quote);
+    @OpenApi(
+            summary = "Create a quote.",
+            operationId = "createQuote",
+            path = "/quotes",
+            method = HttpMethod.POST,
+            tags = {"Quote"},
+            requestBody = @OpenApiRequestBody(
+                    content = {
+                            @OpenApiContent(from = QuoteRequest.class)
+                    }
+            ),
+            responses = {
+                    @OpenApiResponse(
+                            status = "200",
+                            content = {
+                                    @OpenApiContent(
+                                            from = Quote.class
+                                    )
+                            }
+                    ),
+                    @OpenApiResponse(
+                            status = "400",
+                            content = {
+                                    @OpenApiContent(
+                                            from = ErrorResponse.class
+                                    )
+                            }
+                    )
+            }
+
+    )
+    public static void create(Context context) {
+        try {
+            QuoteRequest quoteRequest = context.bodyAsClass(QuoteRequest.class);
+            Quote quote =
+                    QuoteService.save(
+                            quoteRequest.getText(),
+                            quoteRequest.getName()
+                    );
+            context.header("Location", "/quote/" + quote.getId());
+            context.status(HttpCode.CREATED);
+            context.json(quote);
+        } catch (Exception e) {
+            throw new BadRequestResponse();
+        }
     }
 }
